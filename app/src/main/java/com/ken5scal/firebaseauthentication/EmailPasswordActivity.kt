f3443ebecd3e72e79886a17eeca62d1eaca4c1fb
ken5scal <kengoscal@gmail.com>
2017-12-09 20:05:55 +0900

    Implemented Registration method

--- app/src/main/java/com/ken5scal/firebaseauthentication/EmailPasswordActivity.kt
+++ app/src/main/java/com/ken5scal/firebaseauthentication/EmailPasswordActivity.kt
@@ -1,5 +1,38 @@
 package com.ken5scal.firebaseauthentication
 
-/**
- * Created by Kengo on 2017/12/09.
- */
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+
+
+
+class EmailPasswordActivity : AppCompatActivity() {
+    val TAG = "emailActivity"
+    private var mAuth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = mAuth!!.currentUser
+        Log.d(TAG, user.toString())
+    }
+
+    private fun registerAccount(email: String, password: String) {
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = mAuth!!.currentUser
+                        Log.d(TAG, user.toString())
+                        // updateUI(user);
+                    } else {
+                        Log.d(TAG, "createUserWithEmail:failed: ", task.exception)
+                        // updateUI(null);
+                    }
+                }
+    }
+}
