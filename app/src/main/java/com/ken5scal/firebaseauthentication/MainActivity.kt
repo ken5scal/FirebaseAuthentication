956a7ddf04d72a060d03d3892a88e41fa3018fab
ken5scal <kengoscal@gmail.com>
2017-12-10 17:01:37 +0900

    Successfully implemented google sign in

--- app/src/main/java/com/ken5scal/firebaseauthentication/MainActivity.kt
+++ app/src/main/java/com/ken5scal/firebaseauthentication/MainActivity.kt
@@ -1,27 +1,56 @@
 package com.ken5scal.firebaseauthentication
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val AUTH_UI_REQUEST = 123
+    }
+
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener {
-            EmailPasswordActivity.intent(this).let { startActivity(it) }
+//            EmailPasswordActivity.intent(this).let { startActivity(it) }
+            val providers : List<AuthUI.IdpConfig> = listOf(
+                    AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                    AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                    AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+            )
+
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),
+                    AUTH_UI_REQUEST
+            )
         }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var user: FirebaseUser? = mAuth?.currentUser
+        Log.d("UIAuth", user.toString())
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -31,6 +60,14 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AUTH_UI_REQUEST) {
+            val user = FirebaseAuth.getInstance().currentUser
+            Log.d("AuthUIResult -> Success", user.toString())
         }
     }
 }
